bf3a25df84c3a1c1dc4f287daf8d32ab147d0272
Trevor Tombe <trevor.tombe@gmail.com>
2020-12-12 14:33:58 -0700

    update

--- NetCosts_2030.R
+++ NetCosts_2030.R
@@ -79,3 +79,46 @@ ggplot(test,aes(x=x,y=reorder(income,order),fill=..x..))+
        caption="Assumes 2% inflation and uniform behavioural response equivalent to 25% lower emissions from 2022. Graph by @trevortombe")
 ggsave("IncomePlot_2030.png",width=7,height=4.5,dpi=300)
 
+# Share with CTax costs > rebate
+col<-c("#CC2529","#396ab1","#3E9651","#DA7C30","#535154","#6B4C9A","#922428","#948B3D")
+plotdata<-read_excel("CTaxCosts_2030.xlsx",sheet="NetCostwithFed") %>%
+  gather(income,number,-NetCost) %>%
+  group_by(income) %>%
+  filter(NetCost!=1250,income!="All") %>%
+  mutate(share=number/sum(number))  %>%
+  mutate(negative=(NetCost<=0)) %>%
+  group_by(income,negative) %>%
+  summarise(share=sum(share)) %>%
+  ungroup() %>%
+  mutate(income=factor(income,levels=c("<$30k","$30-60k","$60-90k","$90-120k","$120-150k",">$150k")))
+ggplot(plotdata,aes(income,share,group=negative,fill=negative))+
+  geom_col(position='stack')+
+  scale_fill_manual(name="",values=col[1:2],label=c("CTax Costs > Rebate","Rebate > CTax Costs"))+
+  mytheme+
+  scale_y_continuous(label=percent_format(accuracy=1))+
+  labs(x="Total Household Annual Income",y="Share of Households",
+       caption="Graph by @trevortombe",
+       title="How Are Disposable Incomes Affected by the Federal Carbon Tax + Rebate?",
+       subtitle="Source: own calculations from Statistics Canada's SPSM/D version 28.")
+ggsave("disp_income.png",width=8,height=4,dpi=300)
+
+# Plot Urban and Rural Areas
+plotdata<-read_excel("UrbanRural.xlsx",range="A1:E7") %>%
+  select(income=HouseholdIncome,`Cities >500,000`,`Cities <30,000 and Rural Areas`) %>%
+  gather(type,cost,-income) %>%
+  mutate(income=factor(income,levels=c("<$30k","$30-60k","$60-90k","$90-120k","$120-150k",">$150k")))
+ggplot(plotdata,aes(income,cost,group=type,fill=type))+
+  geom_col(position='dodge')+
+  geom_hline(yintercept=0,size=1)+
+  scale_fill_manual(name="",values=col[1:2])+
+  mytheme+
+  scale_y_continuous(label=dollar,limit=c(NA,1000))+
+  labs(x="Total Household Annual Income",y="Annual Carbon Costs, Before Rebate",
+       title="Carbon Tax Costs for Alberta Households ($50/t for 2022)",
+       caption="Graph by @trevortombe",
+       subtitle="Source: own calculations from Statistics Canada's SPSM/D version 28. Excludes rebates.")
+ggsave("rural_urban.png",width=8,height=4,dpi=300)
+  
+
+
+
